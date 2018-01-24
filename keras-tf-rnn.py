9965740c1aacdccc62338d120510fb0527c38ae2
Coultonf <cjrfraser@gmail.com>
2018-01-24 19:00:05 +0100

    Attempted to have the model and weights saved and loaded. Works fine but nothing can save epochs maybe.

--- keras-tf-rnn.py
+++ keras-tf-rnn.py
@@ -3,15 +3,20 @@ import numpy as np
 import time
 import csv
 import re
+import os
+from pathlib import Path
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM, SimpleRNN
 from keras.layers.wrappers import TimeDistributed
 from keras.layers.advanced_activations import LeakyReLU
 from keras.models import load_model
+from keras.callbacks import ModelCheckpoint
+from keras.models import model_from_json
 
 # method for generating text
 def generate_text(model, length, VOCAB_SIZE, ix_to_char):
+
     ix = [np.random.randint(VOCAB_SIZE)]
     y_char = [ix_to_char[ix[-1]]]
     X = np.zeros((1, length, VOCAB_SIZE))
@@ -30,14 +35,13 @@ DATA_DIR = './samples/star_wars.txt'
 BATCH_SIZE = 50
 HIDDEN_DIM = 100
 SEQ_LENGTH = 50
-WEIGHTS = ''
+WEIGHTS = 'keras-model.h5'
 MODE = 'training'
 GENERATE_LENGTH = 100
 LAYER_NUM = 2
 NUM_EPOCH = 10
 
 # Creating training data
-
 #simplify text data for faster processing
 data = re.sub('[^a-z\s]','',re.sub('\s+',' ',open(DATA_DIR, 'r').read()).lower())
 
@@ -76,52 +80,18 @@ for i in range(LAYER_NUM - 1):
   model.add(LSTM(HIDDEN_DIM, return_sequences=True))
 model.add(TimeDistributed(Dense(VOCAB_SIZE)))
 model.add(Activation('softmax'))
+model = load_model('weights.best.h5')
 
 #Compile neural net
 model.compile(loss="categorical_crossentropy", optimizer="Adam", metrics=['accuracy'])
-
-#TODO
-#check to see if previous weights should be loaded
-if not WEIGHTS == '':
-  # print('Loading weights')
-  # # load json and create model
-  # del model
-  # model = load_model('keras-model.h5')
-  # print("Loaded model from disk")
-  # generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
-  # print('\n\n')
-else:
-  NUM_EPOCH = 1
-
-# Training if there is no trained weights specified
-if MODE == 'training' or WEIGHTS == '':
-	while True:
-		print('\n\nEpoch: {}\n'.format(NUM_EPOCH))
-		model.fit(X, y, batch_size=BATCH_SIZE, verbose=1, epochs=1)
-		NUM_EPOCH += 1
-		generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
-		if NUM_EPOCH % 10 == 0:
-			# serialize model to JSON
-			model_json = model.to_json()
-			with open("keras-model.json", "w") as json_file:
-				json_file.write(model_json)
-			# serialize weights to HDF5
-			model.save("keras-model.h5")
-			print("Saved model and weights to disk")
-
-#load the weights and get text
-elif WEIGHTS != '':
-	# Loading the trained weights
-	print('Loading weights')
-	# load json and create model
-	json_file = open('keras-model.json', 'r')
-	loaded_model_json = json_file.read()
-	json_file.close()
-	loaded_model = model_from_json(loaded_model_json)
-	# load weights into new model
-	loaded_model.load_weights("keras-model.h5")
-	print("Loaded model from disk")
-	generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
-	print('\n\n')
-else:
-	print('\n\nNothing to do!')
+generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
+filepath="weights.best.h5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', save_best_only=True, verbose=1, mode='max')
+#model.save()
+callback = ModelCheckpoint(filepath, monitor='val_acc', verbose=0, save_best_only=True, mode='max')
+i = 0
+while i < NUM_EPOCH:
+    print("\n\nEpoch:",i+1)
+    model.fit(X, y, batch_size=BATCH_SIZE ,epochs=1 ,callbacks=[callback], validation_split=0.2)
+    generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
+    i = i + 1
